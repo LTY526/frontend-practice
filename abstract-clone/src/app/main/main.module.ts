d21cfc8f4088a527163de4a336348a8881f72a67
Lai Ting Yang <zzlolzzlol88@gmail.com>
2023-04-07 10:57:32 +0800

    implement functionality of topbar buttons

--- abstract-clone/src/app/main/main.module.ts
+++ abstract-clone/src/app/main/main.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { ContentComponent } from './content/content.component';
 
 
 @NgModule({
   declarations: [
     MainComponent,
-    TopBarComponent
+    TopBarComponent,
+    ContentComponent
   ],
   imports: [
     CommonModule,
--- abstract-clone/src/app/main/top-bar/top-bar.component.ts
+++ abstract-clone/src/app/main/top-bar/top-bar.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./top-bar.component.scss']
 })
 export class TopBarComponent {
+  showOption = false;
+  showSearch = false;
 
+  onToggleOption(): void {
+    this.showOption = !this.showOption;
+  }
+
+  onToggleSearch(): void {
+    this.showSearch = !this.showSearch;
+  }
 }
