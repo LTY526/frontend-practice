46dc4c6dea43b4e6ad31a201f252c99d66992bd7
Lai Ting Yang <zzlolzzlol88@gmail.com>
2023-04-13 10:25:15 +0800

    implement content

--- src/app/lobe-ai-tour/lobe-ai-tour.module.ts
+++ src/app/lobe-ai-tour/lobe-ai-tour.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { LobeAiTourRoutingModule } from './lobe-ai-tour-routing.module';
 import { LobeAiTourComponent } from './lobe-ai-tour.component';
 import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
 
 
 @NgModule({
   declarations: [
     LobeAiTourComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContentComponent
   ],
   imports: [
     CommonModule,
